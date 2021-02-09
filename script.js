88631d2dd4884ed7ca23f0b961ee2622c9cc040e
VenetianRed <60882447+MariuszUrban@users.noreply.github.com>
2021-02-09 14:16:50 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,89 +1,91 @@
-const imageContainer = document.getElementById('image-container'); 
-const loader = document.getElementById('loader');
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
 let photosArray = [];
-let ready = false; 
+let ready = false;
 let imagesLoaded = 0;
 let totalImages = 0;
-// constants for API 
-let countPhotos = 5;
-const apiKEY  = 'xYUWtVaNcw5KT-FpPnBsJGRB4medXk4nXS3iT24-7C0';
-const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKEY}&count=${countPhotos}`
+// constants for API
+let countPhotos = 10;
+const apiKEY = "xYUWtVaNcw5KT-FpPnBsJGRB4medXk4nXS3iT24-7C0";
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKEY}&count=${countPhotos}`;
 //error counter
 let errorCount = 0;
 
-function displayError(){
-    const showAlert = document.createElement('h1');
-    showAlert.textContent = "Rate limit reached, try again soon...";
-    setAttributes(showAlert, {
-        class: "error"
-    })
-    imageContainer.appendChild(showAlert)
+function displayError() {
+  const showAlert = document.createElement("h1");
+  showAlert.textContent = "Rate limit reached, try again soon...";
+  setAttributes(showAlert, {
+    class: "error",
+  });
+  imageContainer.appendChild(showAlert);
 }
 //checker function if all pictures were loaded
 
-function imageLoaded(){
-    imageLoaded++;
-    if(imagesLoaded === totalImages){
-        ready = true;
-        loader.hidden = true;
-    }
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
 }
 
 //helper function to set attributes
 
-function setAttributes(element, attributes){
-    for(const key in attributes){
-        element.setAttribute(key, attributes[key])
-    }
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
 }
 
 // create elements for links, photos
-function displayPhotos(){
-    imagesLoaded = 0;
-    totalImages = photosArray.length; 
-    photosArray.forEach((photo)=>{
-        
-        const link = document.createElement('a');
-        setAttributes(link, {
-            href: photo.links.html,
-            target: '_blank',
-            class: 'link',
-        })
-        
-        const image = document.createElement('img');
-        setAttributes(image,{
-            src: photo.urls.regular,
-            alt: photo.alt_description,
-            title: photo.alt_description,
-        })
-        image.addEventListener('load', imageLoaded())
-        // 
-        link.appendChild(image);
-        imageContainer.appendChild(link)
-    })
+function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  photosArray.forEach((photo) => {
+    const link = document.createElement("a");
+    setAttributes(link, {
+      href: photo.links.html,
+      target: "_blank",
+      class: "link",
+    });
+
+    const image = document.createElement("img");
+    setAttributes(image, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    image.addEventListener("load", imageLoaded());
+    //
+    link.appendChild(image);
+    imageContainer.appendChild(link);
+  });
 }
 
-// get photos from API 
-async function getPictures(){
-    try{
-        const response = await fetch(apiURL);
-        photosArray = await response.json();
-        displayPhotos();
-    }catch (error){
-        errorCount++
-        if(errorCount <= 10){
-            getPictures();
-        }else{
-            displayError()
-        }
+// get photos from API
+async function getPictures() {
+  try {
+    const response = await fetch(apiURL);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (error) {
+    errorCount++;
+    if (errorCount <= 10) {
+      getPictures();
+    } else {
+      displayError();
     }
+  }
 }
 //check to see if scrolling near to bottom
-window.addEventListener('scroll', () =>{
-    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 900 && ready){
-        ready = false;
-        getPictures();
-    }
-})
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 900 &&
+    ready
+  ) {
+    ready = false;
+    getPictures();
+  }
+});
 //on load
 getPictures();
